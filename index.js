e0b196dafb01f5223f9d6d61d250a475afb643b7
Ravi Sawlani <ravisawlani04@gmail.com>
2021-12-28 15:40:46 +0000

    updating the meta tags in the blogs

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-const {md,notion} = require('./config');
-const {uploadToRepo} = require('./github');
+const { md, notion } = require('./config');
+const { uploadToRepo } = require('./github');
 const notionDatabaseId = process.env['NOTION_DATABASE_ID'];
 const githubPath = process.env['GITHUB_FILE_PATH'];
 
 
 async function convertToMd(pageObjects) {
-  const mdBlocks = await Promise.all(pageObjects.map(({id}) => md.pageToMarkdown(id)));
-  const pages = mdBlocks.map((mdBlock,index) => {
+  const mdBlocks = await Promise.all(pageObjects.map(({ id }) => md.pageToMarkdown(id)));
+  const pages = mdBlocks.map((mdBlock, index) => {
     return {
       name: pageObjects[index].name,
-      content: `---\n title: ${pageObjects[index].name}\n date: ${new Date().toISOString()}\n---\n\n `+md.toMarkdownString(mdBlock)
+      content: `---
+  title: ${pageObjects[index].name}
+  date: ${pageObjects[index].date}
+---
+      
+${md.toMarkdownString(mdBlock)}`
+
     }
   });
   return pages;
 }
 
 async function getPublishedPages() {
-  const {results} = await notion.databases.query({
+  const { results } = await notion.databases.query({
     database_id: notionDatabaseId,
     filter: {
       and: [
@@ -28,8 +34,8 @@ async function getPublishedPages() {
         },
         {
           property: 'title',
-          text:{
-            is_not_empty:true
+          text: {
+            is_not_empty: true
           }
         }
       ]
@@ -39,10 +45,12 @@ async function getPublishedPages() {
   const pageObjects = results.map((page) => {
     return {
       id: page.id,
-      name: page.properties['Name'].title[0].plain_text
+      name: page.properties['Name'].title[0].plain_text,
+      date: page.last_edited_time
+      
     }
   });
-   
+
 
   return pageObjects;
 
@@ -52,10 +60,10 @@ async function getPublishedPages() {
 
 getPublishedPages()
   .then(pageIds =>
-  convertToMd(pageIds))
-  .then(data=>{
+    convertToMd(pageIds))
+  .then(data => {
     console.log(data);
     const paths = [];
-    data.forEach(d=>paths.push(`${githubPath}${d.name}.md`));
-    return uploadToRepo(data.map(d=>d.content),paths);
-  }).catch(err=>console.log("[ERROR] ",err));
+    data.forEach(d => paths.push(`${githubPath}${d.name}.md`));
+    return uploadToRepo(data.map(d => d.content), paths);
+  }).catch(err => console.log("[ERROR] ", err));
